2fc3543e94608f63fc16f36288b0f325aec22ee4
pappukr034 <pappukr034@gmail.com>
2023-12-12 17:07:28 +0530

    Practice on DOM has been successfully:-

---
+++ Learning/practice.js
@@ -0,0 +1,16 @@
+        let s1=new String("pappu");
+        let s2=String("kumar");
+        // console.log(typeof s1);
+        // console.log(typeof s2);
+
+        const mySymbol=Symbol("JayShreeRam");
+        console.log(typeof String(mySymbol));
+
+        const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+const word = 'fox';
+
+// console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`,
+// );
+
+console.log(isNaN(90))
